71539e0243d268b060f6077a43c3d57c20258692
Tutorial22 <153890685+Tutorial22@users.noreply.github.com>
2024-01-09 18:59:38 +0530

    Update language.js

--- language.js
+++ language.js
@@ -3,6 +3,7 @@ const chalk = require('chalk')
 
 global.mizolanguage = true //if you put false the bot can talk to English language 
 global.apis = "YybHI6GZ"
+global.owner = "+918416093656"
 global.prefa = ['','!','.','#','&']
 
 let file = require.resolve(__filename)
